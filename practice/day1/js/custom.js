d6d65c3a0333c7cb88aaf74dacaf4642308459ac
Diego Verbrugghe <dverbrugghe@alumnos.exa.unicen.edu.ar>
2011-12-22 10:44:55 -0300

    day 1 - exercises 5 & 6 (missed changes)

--- practice/day1/js/custom.js
+++ practice/day1/js/custom.js
@@ -6,13 +6,35 @@
 
 $(document).ready(function() {
 	showWelcomeMsg();
-	focusAlias();
+	bindAjaxCall();
+	focusAliasInput();
 });
 
 function showWelcomeMsg() {
 	alert('Hello world!');
 }
 
-function focusAlias() {
+function focusAliasInput() {
 	$('#alias').focus();
+}
+
+function bindAjaxCall() {
+	$('#welcomeButton').click(function() {
+		requestWelcome( $('#alias').val() );
+	});
+}
+
+/**
+ * Request welcome service.
+ * 
+ * @param name
+ *            the name of the visitor
+ */
+function requestWelcome(name) {
+	$.post('api/dispatcher.php', {
+		service : 'welcome.hello',
+		params : { 'name' : name }
+	}).done(function(data) {
+		$('#response').html(data);
+	});
 }
