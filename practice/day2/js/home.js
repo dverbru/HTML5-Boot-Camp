5dbc76f0ff856468fb9bcd5c6a17aed6ac05a27e
Diego Verbrugghe <dverbrugghe@alumnos.exa.unicen.edu.ar>
2011-12-23 17:37:30 -0300

    days 2 & 3 - exercises 1 to 6

---
+++ practice/day2/js/home.js
@@ -0,0 +1,54 @@
+/**
+ * Home page code.
+ * 
+ * @author Diego Verbrugghe
+ */
+
+$('#homePage').on('pageinit', function(event) {
+	showInitMsg();
+	bindAjaxCall();
+	focusAliasInput();
+});
+
+function showInitMsg() {
+	alert('Hello world!');
+}
+
+function focusAliasInput() {
+	$('#alias').focus();
+}
+
+function bindAjaxCall() {
+	$('#welcomeButton').click(function() {
+		requestWelcome( $('#alias').val() );
+	});
+}
+
+/**
+ * Request welcome service.
+ * 
+ * @param name
+ *            the name of the visitor
+ */
+function requestWelcome(name) {
+	$.mobile.showPageLoadingMsg();
+	
+	$(document).on('keypress', function(event) {
+		if (event.keyCode === 27) {             // if <Esc> key is pressed,
+			$(document).off('keypress', this);  // unregister this handler
+			$.mobile.hidePageLoadingMsg();      // and hide the message.
+		}
+	});
+
+	div = $('#response');
+	$.post('api/dispatcher.php', {
+		service : 'welcome.hello',
+		params : { 'name' : name }
+	}).done(function(data) {
+		div.html(data);
+		div.removeClass("error");
+	}).fail(function(jqXHR, textStatus, errorThrown) {
+		div.html('[' + textStatus + '] Request failed: ' + errorThrown);
+		div.addClass("error");
+	});
+}
