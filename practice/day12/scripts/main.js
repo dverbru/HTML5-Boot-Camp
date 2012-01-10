62bd1b6fad4beb1eeb6ae39e485335cf8943f513
Diego Verbrugghe <dverbrugghe@alumnos.exa.unicen.edu.ar>
2012-01-10 15:00:35 -0300

    Day 12 - exercise 3

--- practice/day12/scripts/main.js
+++ practice/day12/scripts/main.js
@@ -2,6 +2,32 @@
  * Practice code.
  */
 
+/**
+ * Apply regressive enhancement if native support is not available for certain
+ * features needed. Can load polyfill scripts dinamically.
+ * 
+ * @param callback
+ *            Run this after everything has downloaded and executed
+ */
+function ensureBrowserSupport(callback) {
+	Modernizr.load([
+			// Presentational polyfills
+			{
+				// TODO
+			},
+			// Functional polyfills
+			{
+				test : Modernizr.file && Modernizr.draganddrop,
+				nope : 'scripts/lib/dropfile/dropfile.js',
+				complete : function() {
+					// Run this after everything in this group has downloaded
+					// and executed, as well everything in all previous groups
+					callback();
+				}
+			}
+	]);
+}
+
 function appendScript(code) {
 	var head = document.getElementsByTagName("head")[0];
 	var newScript = document.createElement("script");
@@ -16,6 +42,18 @@ function saveSourceCode(code) {
 
 function restoreSourceCode() {
 	return window.localStorage.getItem('source');
+}
+
+function setupFileDropInHandler(element) {
+	element.addEventListener('drop', function(e) {
+		e.preventDefault();
+
+		var reader = new FileReader();
+		reader.onload = function(evt) {
+			element.value = evt.target.result;
+		};
+		reader.readAsText(e.dataTransfer.files[0]);
+	}, false);
 }
 
 function setupTester() {
@@ -38,17 +76,9 @@ function setupTester() {
 						: "$('#output').html('<h1>Hello World!</h1>');";
 			});
 
-	source.addEventListener('drop', function(e) {
-		e.preventDefault();
-
-		var reader = new FileReader();
-		reader.onload = function(evt) {
-			source.value = evt.target.result;
-		};
-		reader.readAsText(e.dataTransfer.files[0]);
-	}, false);
+	setupFileDropInHandler(source);
 }
 
 $(document).ready(function() {
-	setupTester();
+	ensureBrowserSupport(setupTester);
 });
